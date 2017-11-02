57bdb035a022328fcdfc801b4f904e9f996d5c39
Jesse Atay <jesse.atay@gmail.com>
2017-11-01 18:37:05 -0700

    string reverse

---
+++ string-reverse/script.js
@@ -0,0 +1,13 @@
+const reverseString = (string) => {
+  var newString = string.split("").reverse().join("");
+  return newString;
+}
+
+const printMsg = (message) => {
+document.write("<p>" +  reverseString(message) + "</p>");
+
+}
+printMsg("howdy");
+printMsg("how are you?");
+printMsg("hannah");
+printMsg("1,2,3,4,5,6,7,8,9");
